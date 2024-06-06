edf940f03ab5a31e296128108f8775c5d3d1bd98
DavonLibradilla24 <165020268+DavonLibradilla24@users.noreply.github.com>
2024-06-07 00:25:12 +0530

    teachProf

--- app/src/main/java/com/example/attendacemanagementsystemwonderpets/teachProfile.java
+++ app/src/main/java/com/example/attendacemanagementsystemwonderpets/teachProfile.java
@@ -1,15 +1,23 @@
 package com.example.attendacemanagementsystemwonderpets;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class teachProfile extends AppCompatActivity {
+
+    private static final int REQUEST_GALLERY = 1;
 
     private EditText txtWriteTeachName, txtWriteTeachNum;
     private Button btnSubmitProfile, btnSelectImage;
@@ -38,9 +46,24 @@ public class teachProfile extends AppCompatActivity {
         btnSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code to select image from gallery
+                openGallery();
             }
         });
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            imageProfileView.setImageURI(imageUri);
+        }
     }
 
     private void submitProfile() {
